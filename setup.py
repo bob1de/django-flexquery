34c38f451742d3080bc6c99b587269df938b25f6
Robert Schindler <r.schindler@efficiosoft.com>
2019-07-29 14:36:12 +0200

    Fixed linting

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="django-flexquery",
     version=__version__,
     description="Reusable QuerySet filtering logic for Django, incorporating the DRY principle and maximizing user experience and performance",
-    long_description = open("README.rst").read(),
+    long_description=open("README.rst").read(),
     author="Robert Schindler",
     author_email="r.schindler@efficiosoft.com",
     url="https://github.com/efficiosoft/django-flexquery",
